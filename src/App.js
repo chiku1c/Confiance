7ae1a38b3f9a7e0bcb1dc588d040d257d7d61e74
chiku1c <chiku000c@gmail.com>
2024-05-30 15:43:21 +0530

    task Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Grid from '@mui/material/Unstable_Grid2';
+import ProfileCardComponent from './components/Card';
+
+
 
 function App() {
+  const cardData = [{ id: 1, name: "HR Manger" }, { id: 2, name: "Data Analyst" }, { id: 3, name: "Legal Counsel" }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {cardData.map((index,ele) => (
+          <Grid xs={2} sm={4} md={4} key={index}>
+           <ProfileCardComponent />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,89 @@
+import MuiBox from '@mui/material/Box'
+import MuiCard from '@mui/material/Card'
+import MuiCardContent from '@mui/material/CardContent'
+import MuiCardMedia from '@mui/material/CardMedia'
+import MuiStack from '@mui/material/Stack'
+import MuiTypography from '@mui/material/Typography'
+import Bg from "../assest/image/BG.jpg"
+import Profile from "../assest/image/Profile.jpg"
+
+
+
+const ProfileCardComponent = ({ name }) => {
+    return (
+        <><MuiCard
+            sx={{
+                width: 320,
+                borderRadius: '1rem',
+                boxShadow: '0px 48px 100px 0px rgba(17, 12, 46, 0.1)',
+                zIndex: 0,
+            }}
+        >
+            <MuiCardContent
+                sx={{
+                    position: 'relative',
+                    padding: 0,
+                }}
+            >
+                <MuiCardMedia sx={{ position: 'absolute' }} component="img" image={Bg} />
+            </MuiCardContent>
+
+            <MuiCardContent sx={{ marginTop: '3em' }}>
+                <MuiCardMedia
+                    sx={{
+                        position: 'relative',
+                        width: '50px',
+                        height: '50px',
+                        margin: '8em auto 0 auto', /* 8em top margin, 0 for other margins, auto for horizontal centering */
+                        borderRadius: '100%',
+                        border: '4px solid transparent',
+                        borderColor: 'background.paper',
+                    }}
+                    component="img"
+                    image={Profile}
+                />
+                <MuiTypography
+                    variant="h6" component="h1" color="text.primary" textAlign="center" sx={{
+                        fontWeight: 700,
+                    }} gutterBottom>
+                    Jayvion Simon
+                </MuiTypography>
+                <MuiTypography variant="subtitle1" color="text.secondary" textAlign="center" gutterBottom>
+                    HR manger
+                </MuiTypography>
+                <MuiStack direction="row" justifyContent="space-around" alignItems="center" spacing={2}>
+                    <MuiBox sx={{ textAlign: 'center' }}>
+                        <MuiTypography variant="caption" color="text.secondary">
+                            Following
+                        </MuiTypography>
+                        <MuiTypography variant="h6" color="text.primary" sx={{ fontWeight: 700 }}>
+                            80K
+                        </MuiTypography>
+                    </MuiBox>
+                    <MuiBox sx={{ textAlign: 'center' }}>
+                        <MuiTypography variant="caption" color="text.secondary">
+                            Followers
+                        </MuiTypography>
+                        <MuiTypography variant="h6" color="text.primary" sx={{ fontWeight: 700 }}>
+                            803K
+                        </MuiTypography>
+                    </MuiBox>
+                    <MuiBox sx={{ textAlign: 'center' }}>
+                        <MuiTypography variant="caption" color="text.secondary">
+                            total Post
+                        </MuiTypography>
+                        <MuiTypography variant="h6" color="text.primary" sx={{ fontWeight: 700 }}>
+                            1.4K
+                        </MuiTypography>
+                    </MuiBox>
+                </MuiStack>
+            </MuiCardContent>
+        </MuiCard></>
+
+    )
+}
+
+export default ProfileCardComponent
+
+
+
